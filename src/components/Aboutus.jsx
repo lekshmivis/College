d78ebfad39fd2231efc97e033c59e66b25c9c550
lekshmivis <lekshmipriyas2008@gmail.com>
2024-08-01 16:56:24 -0700

    Completed

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Aboutus = () => {
+  return (
+    <div>
+      <Navbar/>
+        <h1>
+            One of the best colleges in the world
+            <h2>
+                Students from different contries are here
+            </h2>
+        </h1>
+    </div>
+  )
+}
+
+export default Aboutus
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const login = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Name of student </label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">DOB</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Name of the course</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Address</label>
+                                <input type="file" name="" id="" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <input type="radio" name="Male" id="" className="form-check-input" />
+                                <label className="form-check-label">Male</label>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <input type="radio" name="Female" id="" className="form-check-input" />
+                                <label className="form-check-label">Female</label>
+                            </div>
+                            <button className="btn btn-info">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default login
